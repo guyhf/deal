898aeb51d9a59cbf32aa257ee56aa282a69c576b
Guy Haskin Fernald <guyhf@foresitelabs.com>
2022-11-27 20:34:20 -0800

    updates

--- deal.py
+++ deal.py
@@ -7,18 +7,32 @@ choice = None
 cases = []
 
 def switch_screens():
-    global chooseBoxLabel, confirmBox, chooseBoxMenu, deal_button, offer_label
+    global chooseBoxLabel, confirmBox, chooseBoxMenu, deal_button, offer_label, instruction_label
     chooseBoxLabel.pack_forget()
     confirmBox.pack_forget()
     chooseBoxMenu.pack_forget()
     drawCases()
     update_offer()
+
+    spacer.pack()
+    instruction_label.pack()
+
+def offer_screen():
+    global root, offer_label, offer_var
+    for widget in root.winfo_children():
+       widget.destroy()
+    
+    offer_label = tkinter.Label(root,
+        textvariable=offer_var)
     offer_label.pack()
+    deal_button = tkinter.Button(root, text="Deal!", command=make_deal)
     deal_button.pack()
+    no_deal_button = tkinter.Button(root, text="No Deal!", command=make_no_deal)
+    no_deal_button.pack()
 
 
 def choiceCallback(selection):
-    global choice
+    global choice, num_to_choose, instruction_var
     choice = selection
 
 class Case(object):
@@ -49,20 +63,56 @@ class Case(object):
             s = f"[{self.number}]"
         return s
 
+
+def end():
+    root.destroy()
+
 def make_deal():
+    global offer
     newWindow = tkinter.Toplevel(root)
     newWindow.title("Deal is Made!")
  
     # sets the geometry of toplevel
-    newWindow.geometry("300x300")
+    newWindow.geometry("500x500")
  
+    tkinter.Label(newWindow,
+          text =f"You win ${offer}").pack()
+
+    your_case = get_chosen_case()
+    tkinter.Label(newWindow,
+          text =f"Your case had ${your_case.amount}").pack()
+
     # A Label widget to show in toplevel
     tkinter.Label(newWindow,
           text ="Thanks for playing!").pack()
 
+    tkinter.Button(newWindow, text = "Done", command = end).pack()
+
+    
+def make_no_deal():
+    global offer
+    newWindow = tkinter.Toplevel(root)
+    newWindow.title("You chose to keep your case!")
+ 
+    # sets the geometry of toplevel
+    newWindow.geometry("500x500")
+ 
+    your_case = get_chosen_case()
+    tkinter.Label(newWindow,
+          text =f"You win ${your_case.amount}").pack()
+
+    tkinter.Label(newWindow,
+          text =f"You turned down a deal for ${offer}").pack()
+
+    # A Label widget to show in toplevel
+    tkinter.Label(newWindow,
+          text ="Thanks for playing!").pack()
+
+    tkinter.Button(newWindow, text = "Done", command = end).pack()
+
     
 def make_choice():
-    global chooseBoxLabel, confirmBox, chooseBoxMenu, deal_button
+    global chooseBoxLabel, confirmBox, chooseBoxMenu, deal_button, num_to_choose, instruction_var
 
     nrevealed = number_revealed()
     if nrevealed < 8:
@@ -70,10 +120,8 @@ def make_choice():
 
     chooseBoxLabel = tkinter.Label(
         root,
-        text = f"""Welcome to Deal or No Deal!
+        text = """Welcome to Deal or No Deal!
         Choose your case, 1-10 with amounts $1-$1000
-
-        Choose {n_to_reveal} cases to reveal.
         """)
     boxChoice = tkinter.StringVar()
     boxChoiceList = range(1,11)
@@ -82,11 +130,9 @@ def make_choice():
     chooseBoxMenu.pack(expand=True)
     confirmBox = tkinter.Button(root, text = "Confirm", command = switch_screens)
     confirmBox.pack()
-    deal_button = tkinter.Button(root, text="Deal!", command=make_deal)
 
 def drawCases():
     global cases
-    print(f"choice is {choice}")
     for i, amount in enumerate(amounts, 1):
         case = Case(i, amount, update_offer)
         case.button.pack()
@@ -102,6 +148,14 @@ def number_revealed():
         if case.revealed:
             n = n + 1
     return n
+
+
+def get_chosen_case():
+    global cases
+    for case in cases:
+        if case.chosen:
+            return case
+    return None
  
 class Game(object):
 
@@ -125,7 +179,7 @@ class Game(object):
         return "-".join([str(c) for c in self.cases])
 
 def update_offer():
-    global offer_var
+    global offer_var, offer
     total = 0
     count = 0
     for c in cases:
@@ -134,15 +188,20 @@ def update_offer():
             count += 1
     offer = round((total / count) * random.uniform(.8, .9))
     offer_var.set(f"Offer is ${offer}")
-    return offer
+    nrevealed = number_revealed()
+    remaining = num_to_choose - nrevealed
+    if remaining == 0:
+        offer_screen()
+    else:
+        instruction_var.set(f"Choose {remaining}")
+
 
 def setup():
-    global root, amounts, cases, offer_var, offer_label
+    global root, amounts, cases, offer_var, offer_label, instruction_var, instruction_label, spacer, num_to_choose
     root = tkinter.Tk()
     root.title("Deal or No Deal!")
     root.geometry("800x800")
 
-    make_choice()
 
     amounts = [1,2,5,10,25,50,100,250,500,1000]
     random.shuffle(amounts)
@@ -153,7 +212,17 @@ def setup():
     offer_label = tkinter.Label(root,
         textvariable=offer_var)
 
-    cases = []
+    instruction_var = tkinter.StringVar()
+    instruction_var.set("Choose 4 cases")
+
+    instruction_label = tkinter.Label(root,
+        textvariable=instruction_var)
+
+    spacer = tkinter.Label(root, text="", pady=4)
+
+    num_to_choose = 4
+
+    make_choice()
 
 
 setup()
